ef9a31dada7d3125e0ecd468a704cbfcf1903e30
kastashenkova <astashenkova.katya@gmail.com>
2026-03-17 23:26:35 +0200

    fix pr

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,31 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final ConcurrentLinkedQueue<EventListener> listeners
+            = new ConcurrentLinkedQueue<>();
+    private final ExecutorService executor
+            = Executors.newCachedThreadPool();
+
+    public void registerListener(EventListener listener) {
+        listeners.offer(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            executor.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executor.shutdown();
+        listeners.clear();
     }
 }
